2c84608581db4b3cf02bd9092a7216300de492f6
achint <achintj96244@gmail.com>
2023-07-01 11:52:46 +0530

    display x when clicking on a square

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 /* eslint-disable no-unused-vars */
 import Board from './components/Board';
-import { useState } from 'react';
 import './styles.scss';
 function App() {
-  const [counter, setCounter] = useState(1);
-  console.log('hello');
-
-  const onBtnClick = () => {
-    setCounter(currentValue => {
-      return currentValue + 1;
-    });
-  };
-
   return (
     <div className="app">
-      <div>
-        <button onClick={onBtnClick}>Click me</button>
-        <div>{counter}</div>
-      </div>
+      <Board />
     </div>
   );
 }
